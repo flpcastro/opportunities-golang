d5026cba5530afae97229278d968239861535128
Felipe <flpc.dev@gmail.com>
2023-08-22 15:45:24 -0300

    feat: init db and gin router

--- cmd/opportunities/main.go
+++ cmd/opportunities/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/flpcastro/opportunities-api-go/internal/router"
+import (
+	"github.com/flpcastro/opportunities-api-go/internal/configs"
+	"github.com/flpcastro/opportunities-api-go/internal/router"
+	"github.com/flpcastro/opportunities-api-go/pkg/database"
+)
+
+var (
+	logger *configs.Logger
+)
 
 func main() {
+	logger = configs.GetLogger("main")
+
+	// Init DB
+	err := database.Init()
+	if err != nil {
+		logger.Errorf("init database error: %v", err)
+		return
+	}
+
 	// Init router
 	router.Initialize()
 }
