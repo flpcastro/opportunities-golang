58f0f0977883e5ee629bc856bc273fff2145c45a
Felipe <flpc.dev@gmail.com>
2023-08-24 18:20:44 -0300

    feat: create list opportunities handler

--- internal/handler/listOpportunities.go
+++ internal/handler/listOpportunities.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/flpcastro/opportunities-api-go/internal/entity"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpportunitiesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CreateOpportunity",
-	})
+	opportunities := []entity.Opportunity{}
+
+	if err := db.Find(&opportunities).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing opportunities")
+		return
+	}
+
+	sendSuccess(ctx, "list-opportunities", opportunities)
 }
