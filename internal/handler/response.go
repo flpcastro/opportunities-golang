273144b70f900e6e588b7e4da64f782c9778806b
Felipe <flpc.dev@gmail.com>
2023-08-24 13:50:37 -0300

    feat: create request and response structs and validate

---
+++ internal/handler/response.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OpportunityResponse struct {
+	ID        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+}
+
+func sendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation %s successful", op),
+		"data":    data,
+	})
+}
