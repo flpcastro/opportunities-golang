32e530d644ab4c637d833052169a7a126102fb68
Felipe <flpc.dev@gmail.com>
2023-08-24 18:20:26 -0300

    feat: create delete opportunity handler

--- internal/handler/deleteOpportunity.go
+++ internal/handler/deleteOpportunity.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/flpcastro/opportunities-api-go/internal/entity"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CreateOpportunity",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opportunity := entity.Opportunity{}
+
+	// Find Opening
+	if err := db.First(&opportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opportunity with id: %s not found", id))
+		return
+	}
+
+	// Delete Opening
+	if err := db.Delete(&opportunity).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opportunity with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "delete-opportunity", opportunity)
 }
