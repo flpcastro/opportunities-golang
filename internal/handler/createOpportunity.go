e92e3a1a4ce08808ea003618e85bd5c9f275126b
Felipe <flpc.dev@gmail.com>
2023-08-24 13:51:23 -0300

    feat: implement create opportunity endpoint

--- internal/handler/createOpportunity.go
+++ internal/handler/createOpportunity.go
@@ -3,11 +3,35 @@ package handler
 import (
 	"net/http"
 
+	"github.com/flpcastro/opportunities-api-go/internal/entity"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CreateOpportunity",
-	})
+	request := CreateOpportunityRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opportunity := entity.Opportunity{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opportunity).Error; err != nil {
+		logger.Errorf("error creating opportunity: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opportunity on database")
+		return
+	}
+
+	sendSuccess(ctx, "create opportunity", opportunity)
 }
