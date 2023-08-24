e72ea4bf911ffc36d531acecbff03b0a9fa65833
Felipe <flpc.dev@gmail.com>
2023-08-24 18:20:58 -0300

    feat: create show opportunity handler

--- internal/handler/showOpportunity.go
+++ internal/handler/showOpportunity.go
@@ -3,11 +3,21 @@ package handler
 import (
 	"net/http"
 
+	"github.com/flpcastro/opportunities-api-go/internal/entity"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "CreateOpportunity",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opportunity := entity.Opportunity{}
+	if err := db.First(&opportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opportunity not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opportunity", opportunity)
 }
