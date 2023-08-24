ce777750bfd8ad203b6ec7033a67ce4d1f13db67
Felipe <flpc.dev@gmail.com>
2023-08-24 13:51:00 -0300

    feat: init handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"github.com/flpcastro/opportunities-api-go/internal/configs"
+	"github.com/flpcastro/opportunities-api-go/pkg/database"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *configs.Logger
+	db     *gorm.DB
+)
+
+func InitHandler() {
+	logger = configs.GetLogger("handler")
+	db = database.GetPostgreSQL()
+}
